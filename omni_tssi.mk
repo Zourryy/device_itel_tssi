1c1ba685ac7e2f8bc33a513c3486f2a250e99088
dyy <muhamadtriyadi897@gmail.com>
2026-09-05 09:33:34 +0700

    Remove vendor/omni dependency not present in minimal manifest

--- omni_tssi.mk
+++ omni_tssi.mk
@@ -10,7 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from tssi device
 $(call inherit-product, device/itel/tssi/device.mk)
